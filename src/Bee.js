ae1031168e855e6a0dc08458c410209054ab6a23
emvh <emilyvho@gmail.com>
2020-05-18 11:03:50 -0700

    Create Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,11 +1,12 @@
 var Bee = function() {
   Grub.call(this);
-  this.age = 5;
-  this.color = 'yellow';
-  this.job = function() {
-  //   console.log('keep on growing');
-  // }
 };
 
-// Bee.prototype = Object.create(Grub.prototype);
-// Bee.prototype.constructor = Bee;
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+Bee.prototype = {
+  age: 5,
+  color: 'yellow',
+  job: 'keep on growing';
+  }
+};
