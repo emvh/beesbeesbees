695e529699e2a1ef852c9b2abc1186e86c992601
emvh <emilyvho@gmail.com>
2020-05-18 10:59:54 -0700

    Create grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
 var Grub = function() {
 };
 
+Grub.prototype.constructor = Grub;
+Grub.prototype = {
+  age: 0,
+  color: 'pink',
+  food: 'jelly',
+  eat: function(){
+    console.log('nom!');
+}
+
