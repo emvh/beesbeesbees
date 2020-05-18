ac7e9a10944a6f4a936868e796cff69ea186aa46
emvh <emilyvho@gmail.com>
2020-05-18 13:18:58 -0700

    Complete Bees repo tests

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -15,15 +15,15 @@ RetiredForagerBee.prototype.forage = function() {
 RetiredForagerBee.prototype.gamble = function(treasure) {
   this.treasureChest.push(treasure);
 
-// // RetiredForagerBee.prototype = {
-// //   // ForagerBee.call(this),
-// //   constructor: RetiredForagerBee,
-// //   age: 40,
-// //   job: 'gamble',
-// //   canFly: false,
-// //   color: grey,
-// //   forage: 'I am too old, let me play cards instead',
-// //   gamble: function(treasure) {
-// //     this.treasureChest.push(treasure);
-// //   }
-// // };
+// RetiredForagerBee.prototype = {
+//   // ForagerBee.call(this),
+//   constructor: RetiredForagerBee,
+//   age: 40,
+//   job: 'gamble',
+//   canFly: false,
+//   color: grey,
+//   forage: 'I am too old, let me play cards instead',
+//   gamble: function(treasure) {
+//     this.treasureChest.push(treasure);
+//   }
+// };
